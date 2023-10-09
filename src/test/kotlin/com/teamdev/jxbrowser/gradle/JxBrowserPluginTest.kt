a6cce69c9647a17ec9b5ff7938c17513359a315c
Danil Didkovskiy <danylo.didkovskyi@teamdev.com>
2023-10-09 19:55:51 +0300

    Add copyright

--- src/test/kotlin/com/teamdev/jxbrowser/gradle/JxBrowserPluginTest.kt
+++ src/test/kotlin/com/teamdev/jxbrowser/gradle/JxBrowserPluginTest.kt
@@ -1,3 +1,23 @@
+/*
+ *  Copyright 2023, TeamDev. All rights reserved.
+ *
+ *  Redistribution and use in source and/or binary forms, with or without
+ *  modification, must retain the above copyright notice and the following
+ *  disclaimer.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.teamdev.jxbrowser.gradle
 
 import com.teamdev.jxbrowser.gradle.JxBrowserPlugin.Companion.EAP_REPOSITORY_URL
@@ -25,7 +45,7 @@ class JxBrowserPluginTest {
     }
 
     @Test
-    fun `extension is created properly`() {
+    fun `plugin extension is created properly`() {
         assertDoesNotThrow { project.extensions.getByName("jxbrowser") }
         assertFalse(extension.includePreviewBuilds)
         assertEquals(Repository.NORTH_AMERICA, extension.repository)
