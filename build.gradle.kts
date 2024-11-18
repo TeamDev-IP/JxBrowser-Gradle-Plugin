444391a6e4f878e2bab4f19a09445c3ca2aa1642
Vladimir Ikryanov <vladimir.ikryanov@teamdev.com>
2024-11-18 19:52:22 +0200

    Update JxBrowser version to 8.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object BuildSettings {
     const val GROUP = "com.teamdev.jxbrowser"
     const val VERSION = "1.2.1"
-    const val JXBROWSER_VERSION = "8.0.0"
+    const val JXBROWSER_VERSION = "8.1.0"
     val javaVersion = JavaVersion.VERSION_1_8
 }
 
