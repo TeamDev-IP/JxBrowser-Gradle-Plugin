f1ca9faff830f24aa044ef8f26aeb00783c2cf30
Vladyslav Lubenskyi <Vladyslav.lubenskyi@teamdev.com>
2026-04-27 12:19:28 +0200

    Bump version to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object BuildSettings {
     const val GROUP = "com.teamdev.jxbrowser"
-    const val VERSION = "2.0.0"
+    const val VERSION = "2.1.0"
     const val JXBROWSER_VERSION = "9.0.0"
     val javaVersion = JavaVersion.VERSION_1_8
 }
